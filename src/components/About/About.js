f629df791dabe6ba1964e9c202b72bd3f69523a3
smahkafi <sakafi.m@gmail.com>
2021-10-04 13:45:55 +0600

    about content write

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>about</h2>
+        <div className="container">
+            <h2 className="text-center">
+                About Us
+            </h2>
+            <h2>
+            Take free online courses in solar, nuclear and other forms of energy. Learn about worldwide energy development and the impact of energy production and use on the environment.
+            </h2>
+            
+            <h3>
+                <span className="text-info pe-2">
+                    Related topics:
+                </span> 
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Agriculture</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Biorefinery</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Chemistry</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Sciences</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Earth</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Electric Cars</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Electricity</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Nature</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Oil</Link>
+                    </span> |  
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Solar Energy</Link>
+                    </span> | 
+                    <span className="related">
+                        <Link  to="/" className=" related2 text-secondary">Nuclear Energy</Link>
+                    </span>
+            </h3>
         </div>
     );
 };
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Banner from '../Banner/Banner';
 
 const Features = () => {
     const [features, setFeatures] = useState([])
@@ -10,10 +11,20 @@ const Features = () => {
     console.log(setFeatures);
     return (
         <div>
+            <Banner></Banner>
            { 
             features.map(feature => <div>
                 <div className="home">
-                    
+                    <div className="card" style={{width: "18rem"}}>
+                        <div className="img">
+                        <img src={feature.img} className="card-img-top" alt="..."/>
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button>Go Anywhare</button>
+                        </div>
+                    </div>
                 </div>
 
             </div>)
